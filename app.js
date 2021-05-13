703b20b51349da0c0de44872ce9080c55c83d5e0
wyattDockstader <81665137+wyattDockstader@users.noreply.github.com>
2021-05-13 14:03:08 -0600

    update

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 
 var health = 100
+var name = "name"
+var hits = 0
+
+updateHit()
+updateHealth()
 
 function slap() {
+    hits = hits + 1
     health -= 1
-    update()
+    updateHealth()
+    updateHits()
 }
-function update() {
+function punch() {
+    health -= 5
+    hits += 1
+    updateHealth()
+    updateHits()
+}
+function kick() {
+    health -= 10
+    hits += 1
+    updateHealth()
+    updateHits()
+}
+
+
+function updateHealth() {
     let template = health
     let healthElem = document.getElementById("health")
     healthElem.innerHTML = template
+}
+function updateHit() {
+    let template = hits
+    let hitElem = document.getElementById("hits")
+    hitElem.innerHTML = template
 }
